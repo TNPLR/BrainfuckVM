72e91c9f511c95528503ad024cccfde362fee36e
TNPLR <hsiaosvideo@gmail.com>
2018-08-08 20:39:46 +0800

    It can run well except "[]" loop.

---
+++ Makefile
@@ -0,0 +1,9 @@
+CC=gcc
+CFLAG=-O3 -c
+
+all: main.o
+	gcc main.o -o bvm
+main.o:
+	${CC} ${CFLAG} main.c -o main.o
+clean:
+	rm -f *.o
---
+++ main.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+void vm(FILE *fPtr);
+int main(int argc, char* argv[]) {
+  if (argc < 2) {
+    printf("You did not input any files.\n");
+    exit(1);
+  }
+  FILE *fPtr;
+  char c;
+  fPtr = fopen(argv[1], "r");
+  if (!fPtr) {
+    printf("Cannot read this file.\n");
+    exit(1);
+  }
+  vm(fPtr);
+  printf("\n"); 
+  fclose(fPtr);
+
+  return 0;
+}
+void vm(FILE *fPtr) {
+  char c;
+  int mem_length = 1024;
+  int while_loop_length = 16;
+  int *memory = calloc(mem_length, sizeof(int));
+  long *while_mem = malloc(while_loop_length * sizeof(long));
+  int while_nested = 0;
+  if (!memory) {
+    printf("FATAL: Cannot alloc memory.\n");
+    exit(1);
+  }
+  int mem_pos = 0;
+  int while_loop_start = -1;
+  while ((c = fgetc(fPtr)) != EOF) {
+/*
+    if (jmp == 1 && c = ']') {
+      jmp = 0;
+      --while_nested;
+      --while_mem;
+      if (memory[mem_pos]){
+        fseek(fPtr, *while_mem, SEEK_SET);
+        ++while_mem;
+      }
+    }
+*/
+    switch (c) {
+      case '+':
+        ++(memory[mem_pos]);
+        break;
+      case '-':
+        --(memory[mem_pos]);
+        break;
+      case '>':
+        ++mem_pos;
+        if (mem_pos  >= (mem_length - 1)) {
+          mem_length += 1024;
+          memory = realloc(memory, sizeof(int) * mem_length);
+        }
+        break;
+      case '<':
+        if (mem_pos <= 0) {
+          printf("FATAL: Memory error\n");
+          exit(1);
+        } else {
+          --mem_pos;
+        }
+        break;
+      case '.':
+        putchar(memory[mem_pos]);
+        break;
+      case ',':
+        memory[mem_pos] = getchar();
+        break;
+/*
+      case '[':
+        ++while_nested;
+        while_mem->pos = ftell(fPtr);
+        if (!memory[mem_pos]) {
+           while_mem->jmp = 1;
+        }
+        ++while_mem;
+        break;
+      case ']':
+        --while_nested;
+        --while_mem;
+        if (while_mem->jmp == 1) {
+          break;
+        } else if (memory[mem_pos]){
+          fseek(fPtr, *while_mem, SEEK_SET);
+          ++while_mem;
+        }
+        break;
+*/
+      default:
+        break;
+    }
+  }
+}
